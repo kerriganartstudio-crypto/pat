62748a566bb38b283e544df6a244df44e59df7c7
kerriganartstudio-crypto <kerriganartstudio@gmail.com>
2026-04-03 13:50:40 -0500

    sw.js

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = 'kerrigan-os-v1';
+const urlsToCache = [
+  '/pat/index.html'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
